54013164c6499afa8e6043bdaa74c53412fde194
Ubuntu <ubuntu@ip-172-31-41-158.ap-southeast-1.compute.internal>
2023-06-17 16:09:44 +0000

    added templates

--- app.py
+++ app.py
@@ -17,59 +17,28 @@ dynamodb = boto3.resource(
 from boto3.dynamodb.conditions import Key, Attr
 
 @app.route('/')
-def index():
-    #dynamodb_ct.create_table()
-    #return 'Table Created'
-    return render_template('index.html')
-
-
-@app.route('/signup', methods=['post'])
-def signup():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
-        
-        table = dynamodb.Table('users')
-        
-        table.put_item(
-                Item={
-        'name': name,
-        'email': email,
-        'password': password
-            }
-        )
-        msg = "Registration Complete. Please Login to your account !"
-    
-        return render_template('login.html',msg = msg)
-    return render_template('index.html')
-
-@app.route('/login')
-def login():    
+def default():
     return render_template('login.html')
+    
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+    
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
 
+@app.route('/profile-edit.html')
+def update():
+    return render_template('profile-edit.html')
 
-@app.route('/check',methods = ['post'])
-def check():
-    if request.method=='POST':
-        
-        email = request.form['email']
-        password = request.form['password']
-        
-        table = dynamodb.Table('users')
-        response = table.query(
-                KeyConditionExpression=Key('email').eq(email)
-        )
-        items = response['Items']
-        name = items[0]['name']
-        print(items[0]['password'])
-        if password == items[0]['password']:
-            
-            return render_template("home.html",name = name)
-    return render_template("login.html")
-@app.route('/home')
-def home():
-    return render_template('home.html')
+@app.route('/profile-view.html')
+def view():
+    return render_template('profile-view.html')
+    
+@app.route('/search.html')
+def search():
+    return render_template('search.html')
 
 
 
